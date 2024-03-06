603c066a9b2e57179e4d2f875bcc3ac0b4c01279
玮彬 王 <1756355001@qq.com>
2024-03-06 15:08:47 +0800

    2024.3.6

---
+++ assignment/assignment2/assignment2-1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace assignment2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine("请输入数组（中间用空格隔开）");
+            try
+            {
+                s = Console.ReadLine();
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+            string[] nums = s.Split(" ");
+            Console.WriteLine("这些数中的素数有：");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int tmp = int.Parse(nums[i]);
+                if (judge(tmp))
+                {
+                    Console.Write(tmp+" ");
+                }
+            }
+            
+        }
+        static bool judge(int num)
+        {
+            if (num == 1)
+            {
+                return false;
+            }else if(num == 2)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 2;i <= Math.Sqrt(num);i++)
+                {
+                    if (num % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } 
+    }
+}
