30206e443b9bfc3f7c9fb7aae6352997a53b4bc2
玮彬 王 <1756355001@qq.com>
2024-03-08 15:20:07 +0800

    2024.3.8

--- assignment/assignment2/assignment2-4/Program.cs
+++ assignment/assignment2/assignment2-4/Program.cs
@@ -17,12 +17,39 @@
                 string[] arrs = s.Split(" ");
                 for (int j = 0; j < arrs.Length; j++)
                 {
-                    arr[i, j] = int.Parse(arrs[i]);
+                    arr[i, j] = int.Parse(arrs[j]);
                 }
             }
 
-
-
+            for (int i = 0;i < m; i++)
+            {
+                if (!judge(m, n, i, 0, arr))
+                {
+                    Console.WriteLine("该矩阵不符合要求");
+                    return;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (!judge(m, n, 0, i, arr))
+                {
+                    Console.WriteLine("该矩阵不符合要求");
+                    return;
+                }
+            }
+            Console.WriteLine("该矩阵符合要求");
+        }
+        static bool judge(int m,int n,int x,int y, int[,] arr)
+        {
+            int tar = arr[x,y];
+            for (int i = x + 1, j = y + 1; i < m && j < n; i++, j++)
+            {
+                if (arr[i,j]!= tar)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
